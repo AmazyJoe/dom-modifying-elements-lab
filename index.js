49a6d15506f18cc80235602e9cf2399834985b93
AmazyJoe <ochiengolu@gmail.com>
2022-12-04 08:14:45 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//const element=document.createElement('Div')
+//document.body.append(element)
+//const ul = document.createElement("ul");
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Joseph is the champion"
